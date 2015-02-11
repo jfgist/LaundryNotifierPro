2516cd110551b6d602b64535baae28929815a7c9
jfgist <James.Gist@gmail.com>
2015-02-10 21:10:52 -0700

    Update ListenAndEmail.py

--- ListenAndEmail.py
+++ ListenAndEmail.py
@@ -12,18 +12,6 @@ print("GPIO Setup")
 inputPin = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(inputPin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-logFile = open("./log1.dat","w");
-
-# Setup email timer delay
-delay = 3 # default to one hour
-print delay
-
-if len(sys.argv) > 1:
-  print delay
-  print('here')
-  delay = int(sys.argv[1])
-
-print(delay)
 
 # read the to email, from email and from password from config file
 emailConfigFile ='/home/pi/tools/email.conf'
@@ -41,46 +29,24 @@ print(gmail_password)
 today = datetime.date.today()
 buttonPressed = False
 
-dateString = '%H:%M:%S'
-
 while True:
   #take a reading
   input = GPIO.input(inputPin)
-  line = str(input) +"," + datetime.datetime.now().strftime(dateString)
-  print(line)
-  logFile.write(line+ '\n')
-  time.sleep(0.5)
- 
-#  if(input and not(buttonPressed)):
-#    print('Button Pressed First')
-#    time.sleep(1)
-#    input = GPIO.input(inputPin)
-#    if (input):
-#      print("Button held for X seconds")
-#      #buttonPressed = True
-#      buttonPressTime = time.time()
-#    else:
-#      print("Button Not Held Down")
-#    time.sleep(1)
 
-  if (buttonPressed):
-    print("Button Pressed True")
-    print((time.time()) - buttonPressTime)
-    print(delay)
-    if ((time.time() - buttonPressTime) > delay):
-      print("Send Email")
-      msg = MIMEText('ROTATE YOUR LAUNDRY')
-      msg['Subject'] = 'Pi-Notify on %s' % today.strftime('%b %d %Y')
-      msg['From'] = gmail_user
-      msg['To'] = to
-      smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
-      smtpserver.ehlo()
-      smtpserver.starttls()
-      smtpserver.ehlo
-      smtpserver.login(gmail_user, gmail_password)
-      smtpserver.sendmail(gmail_user, [to], msg.as_string())
-      smtpserver.close()
+  if (input):
+    print("Send Email")
+    msg = MIMEText('ROTATE YOUR LAUNDRY')
+    msg['Subject'] = 'Pi-Notify on %s' % today.strftime('%b %d %Y')
+    msg['From'] = gmail_user
+    msg['To'] = to
+    smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo
+    smtpserver.login(gmail_user, gmail_password)
+    smtpserver.sendmail(gmail_user, [to], msg.as_string())
+    smtpserver.close()
+    time.sleep(60);
       
-      buttonPressed = False
 
 
